c006b4e28d8722c4e075c6cbcf3109cd4478af49
elyor0707-cloud <elyor0707@gmail.com>
2026-02-17 09:01:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
 import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from database import init_db, get_user
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_HOST = os.getenv("RENDER_EXTERNAL_HOSTNAME")
-WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
-WEBHOOK_URL = f"https://{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = int(os.getenv("PORT", 10000))
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
+init_db()
+
 @dp.message_handler(commands=['start'])
 async def start_cmd(message: types.Message):
-    await message.answer("Бот ишлаяпти 🚀")
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-async def on_shutdown(dp):
-    await bot.delete_webhook()
+    get_user(message.from_user.id)
+    await message.answer("📖 Қуръон ёдлаш платформасига хуш келибсиз.")
 
 if __name__ == "__main__":
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dp, skip_updates=True)
