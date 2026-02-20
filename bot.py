d9bb0c3e4c3886c00a80c39615947128e76785c2
elyor0707-cloud <elyor0707@gmail.com>
2026-02-20 14:10:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -350,8 +350,7 @@ async def send_ayah(user_id, message):
     surah_name = r['data'][0]['surah']['englishName']
     total_ayahs = r['data'][0]['surah']['numberOfAyahs']
 
-    def create_card_image(arabic_html, translit, surah_name, ayah):
-
+    
     await loading.delete()
     await message.answer_photo(InputFile("card.png"))
 
