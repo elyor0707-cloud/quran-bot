f2ed30b82678c73089da8a3618f6c193844267f5
elyor0707-cloud <elyor0707@gmail.com>
2026-02-21 16:16:50 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -499,32 +499,32 @@ async def play_surah(callback: types.CallbackQuery):
 
     # 🔥 Фақат Mishary ишлайдиган сервер
     @dp.callback_query_handler(lambda c: c.data.startswith("play|"))
-async def play_surah(callback: types.CallbackQuery):
+    async def play_surah(callback: types.CallbackQuery):
 
-    await callback.answer("⏳ Yuklanmoqda...")
+        await callback.answer("⏳ Yuklanmoqda...")
 
-    _, reciter, surah_id = callback.data.split("|")
-    surah_id = int(surah_id)
+        _, reciter, surah_id = callback.data.split("|")
+        surah_id = int(surah_id)
 
-    sura = str(surah_id).zfill(3)
+        sura = str(surah_id).zfill(3)
 
-    FOLDERS = {
-        "Alafasy_128kbps": "Alafasy_128kbps",
-        "Badr_AlTurki_128kbps": "Badr_AlTurki_128kbps",
-        "Alijon_Qori_128kbps": "Alijon_Qori_128kbps"
-    }
+        FOLDERS = {
+            "Alafasy_128kbps": "Alafasy_128kbps",
+            "Badr_AlTurki_128kbps": "Badr_AlTurki_128kbps",
+            "Alijon_Qori_128kbps": "Alijon_Qori_128kbps"
+        }
 
-    folder = FOLDERS.get(reciter)
+        folder = FOLDERS.get(reciter)
 
-    if not folder:
-        await callback.message.answer("Qori topilmadi ❌")
-        return
+        if not folder:
+            await callback.message.answer("Qori topilmadi ❌")
+            return
 
-    audio_url = f"https://everyayah.com/data/{folder}/{sura}001.mp3"
+        audio_url = f"https://everyayah.com/data/{folder}/{sura}001.mp3"
 
-    await callback.message.answer_audio(audio=audio_url)
+        await callback.message.answer_audio(audio=audio_url)
 
-    await callback.message.answer_audio(audio=audio_url)
+        await callback.message.answer_audio(audio=audio_url)
     
 @dp.callback_query_handler(lambda c: c.data.startswith("qori_"))
 async def select_qori(callback: types.CallbackQuery):
