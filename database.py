c95638a13fc65862e0d9856684cafccaa6983603
elyor0707-cloud <elyor0707@gmail.com>
2026-02-23 17:46:24 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 # database.py
 
-users = {}
+# Foydalanuvchilar ma'lumotlari
+USERS = {}
 
-def get_surahs():
-    surah_names = [
+# Suralar ro‘yxati
+SURAH_LIST = [
+    {"number": i + 1, "name": name} for i, name in enumerate([
         "Fatiha","Baqara","Ali Imran","Nisa","Maida","Anam","Araf","Anfal","Tawba","Yunus",
         "Hud","Yusuf","Rad","Ibrahim","Hijr","Nahl","Isra","Kahf","Maryam","Taha",
         "Anbiya","Hajj","Muminun","Nur","Furqan","Shuara","Naml","Qasas","Ankabut","Rum",
@@ -16,39 +18,34 @@ def get_surahs():
         "Shams","Layl","Duha","Sharh","Tin","Alaq","Qadr","Bayyina","Zalzala","Adiyat",
         "Qaria","Takathur","Asr","Humaza","Fil","Quraysh","Maun","Kawthar","Kafirun","Nasr",
         "Masad","Ikhlas","Falaq","Nas"
-    ]
+    ])
+]
 
-    return [
-        {"number": i + 1, "name": surah_names[i]}
-        for i in range(len(surah_names))
-    ]
-
-
-
+def get_surahs():
+    return SURAH_LIST
 
 def get_user(user_id):
-    if user_id not in users:
-        users[user_id] = {
+    if user_id not in USERS:
+        USERS[user_id] = {
+            "user_id": user_id,
             "current_surah": 1,
-            "current_ayah": 1
+            "current_ayah": 1,
+            "is_premium": False,
+            "last_surah": None,
+            "last_ayah": None,
+            "last_page": None
         }
-    return users[user_id]
+    return USERS[user_id]
 
-def update_user(user_id, field, value):
-    if user_id not in users:
-        get_user(user_id)
+def update_user(user_id, key, value):
+    user = get_user(user_id)
+    user[key] = value
+    USERS[user_id] = user
 
-    users[user_id][field] = value
+def get_premium_users():
+    return [u for u in USERS.values() if u.get("is_premium", False)]
 
-
-def get_ayah(surah, ayah):
-    # TEST VERSION (кейин API'га улаймиз)
-    return {
-        "surah_name": f"{surah}-сура",
-        "arabic": f"Оят {ayah} арабча матн",
-        "uzbek": f"Оят {ayah} ўзбекча таржима",
-        "total_ayahs": 7
-    }
+# Progress funksiyalari
 def update_progress(user_id, surah, ayah):
     update_user(user_id, "last_surah", surah)
     update_user(user_id, "last_ayah", ayah)
@@ -63,25 +60,3 @@ def update_page_progress(user_id, page):
 def get_page_progress(user_id):
     user = get_user(user_id)
     return user.get("last_page")
-
-
-def get_user(user_id):
-    if user_id not in USERS:
-        USERS[user_id] = {
-            "user_id": user_id,
-            "current_surah": 1,
-            "current_ayah": 1,
-            "is_premium": False
-        }
-    return USERS[user_id]
-
-def update_user(user_id, key, value):
-    user = get_user(user_id)
-    user[key] = value
-    USERS[user_id] = user
-
-def get_surahs():
-    return SURAH_LIST
-
-def get_premium_users():
-    return [u for u in USERS.values() if u.get("is_premium", False)]
